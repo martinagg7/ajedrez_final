dada876a9ba9a2157c61386350186c95b37ce4eb
Martina Garcia <martinagg@myuax.com>
2022-12-12 23:31:31 +0100

    .

--- imprimirtablero.py
+++ imprimirtablero.py
@@ -8,6 +8,15 @@ tablero = [
     ['♙',	'♙',	'♙',	'♙',	'♙',	'♙',	'♙',	'♙'],
     ['♖',	'♘',	'♗',	'♕',	'♔',	'♗',	'♘',	'♖'],
 ]
+
 def imprimir_tablero(tablero):
-    for k in tablero :
+    i=0
+    cabecera= ['A','B','C','D','E','F','G','H']
+    print('Este es el tablero inicial:')
+    print(' ',cabecera)
+    for k in tablero:
+        i+=1
+        print(i,end=' ')
         print(k)
+
+imprimir_tablero()
